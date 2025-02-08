61f19d96123bd2cc3c8b2be7d3ea2d73b12489e3
Stéphan Puget <spu@ivalua.com>
2025-02-08 14:00:35 +0100

    Update

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,14 +1,15 @@
 import pandas as pd
-from sklearn.metrics import silhouette_score, davies_bouldin_score
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 
 def evaluate_clustering(df):
     if "cluster" in df.columns and df["cluster"].nunique() > 1:
-        features = df.drop(columns=["cluster"])
+        features = df.drop(columns=["cluster", "client_id"])
         labels = df["cluster"]
         silhouette = silhouette_score(features, labels)
         davies_bouldin = davies_bouldin_score(features, labels)
-        return {"Silhouette Score": silhouette, "Davies-Bouldin Score": davies_bouldin}
-    return {"Silhouette Score": None, "Davies-Bouldin Score": None}
+        chi_score = calinski_harabasz_score(features, labels)
+        return {"Silhouette Score": silhouette, "Davies-Bouldin Score": davies_bouldin, "Calinski Harabasz Score": chi_score}
+    return {"Silhouette Score": None, "Davies-Bouldin Score": None, "Calinski Harabasz Score": None}
 
 if __name__ == "__main__":
     df = pd.read_csv("clustered_data.csv")
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,20 +1,40 @@
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
 
 def preprocess_data(df):
-    # Drop unnecessary columns
-    df = df.drop(columns=["Unnamed: 0"], errors="ignore")
+
+    # Drop unnecessary, redundant and low variance columns
+    df = df.drop(columns=["Unnamed: 0", "most_buyed_product", "most_ordered_product_1", "most_ordered_product_2", "most_ordered_product_3", "favorite_prd_category_1", "favorite_prd_category_3", "favorite_prd_category_4", "agence_code", "brand_name", "nb_different_product_ordered", "purchase_activity_rate", "interaction_rate"], errors="ignore")
     df = df.rename(columns={"type": "client_type"})
-    
+
+    # Filling missing values
+    for col in ['client_type', 'market']:
+        mode_value = df[col].mode()[0]
+        df[col] = df[col].fillna(mode_value)
+
+    # Fix skewed columns
+    skewed_cols = ["average_time_between_searches_days", "avg_products_seen", "nb_total_orders", "avg_quantity_per_order", "unique_prd_category_count"]
+    for col in skewed_cols:
+        df[col] = np.log1p(df[col])
+        upper_percentile = df[col].quantile(0.95)
+        df[col] = df[col].clip(upper=upper_percentile)
+
+
     # Encode categorical features
-    categorical_cols = df.select_dtypes(include=["object"]).columns
-    encoder = OrdinalEncoder()
-    df[categorical_cols] = encoder.fit_transform(df[categorical_cols])
+    one_hot_encoding_columns = ["device_usage_category", "client_type", "market", "preferred_channel"]
+    other_encoding_columns = ["favorite_prd_category_2"]
+    df_one_hot = pd.get_dummies(df[one_hot_encoding_columns], drop_first=True)
+    ord_encoder = OrdinalEncoder()
+    df_ordinal = df[other_encoding_columns].copy()
+    df_ordinal[other_encoding_columns] = ord_encoder.fit_transform(df_ordinal)
+    df = df.drop(columns=one_hot_encoding_columns + other_encoding_columns)
+    df = pd.concat([df, df_one_hot, df_ordinal], axis=1)
     
     # Normalize numerical features
-    scaler = StandardScaler()
-    numerical_cols = df.select_dtypes(include=["number"]).columns
-    df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+    feature_cols = df.drop('client_id', axis=1).columns
+    scaler = MinMaxScaler()
+    df[feature_cols] = scaler.fit_transform(df[feature_cols])
     
     return df
 
